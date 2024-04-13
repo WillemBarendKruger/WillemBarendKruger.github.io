e621239cb67fdf948a7671daf633992dc851a302
Willem Barend Kruger <willem.kruger11@gmail.com>
2024-04-13 14:36:09 +0200

    First upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+function Menufunction(){
+    document.getElementById("MenuList").innerHTML(<ul>
+        <li>Home</li>
+        <li>Contact Me</li>
+        <li>Download Resume</li>
+        </ul>)
+}
