19cb0cd096ddae3711df48703e3734af3582c47a
Willem Barend Kruger <willem.kruger11@gmail.com>
2024-05-19 13:22:10 +0200

    New site
    Loaded the new template slide with some updates

--- Script.js
+++
@@ -1,7 +0,0 @@
-function Menufunction(){
-    document.getElementById("MenuList").innerHTML(<ul>
-        <li>Home</li>
-        <li>Contact Me</li>
-        <li>Download Resume</li>
-        </ul>)
-}
